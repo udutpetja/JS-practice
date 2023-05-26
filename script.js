5286a6aed8c971a5145d58d3c867c98067346d46
Petro <example@gmail.com>
2023-05-27 00:09:47 +0300

    Second practice if else switch

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
- const numberOfFilms = +prompt('How much movie you are already watched?', '');
+"use strict";
+
+/*  const numberOfFilms = +prompt('How much movie you are already watched?', ''); це модальне вілкно з запитанням
 
  const personalMovieDB = {
     count: numberOfFilms,
@@ -14,7 +16,43 @@
         d = prompt('How many stars you can give to this movie?', '');
 
 
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = b; в дерективу мовіДБ а саме в мові буде записано відповіді з а по с
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); 
+*/
+
+
+/* if (4 == 4) {
+    console.log('OK!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too mush');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error'); 
+// якщо число 50 тоді ок якщо ні тоді помилка (тернарний оператор) */
+
+const num = 20;
+switch (num) {
+    case 49: 
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 50:
+        console.log('OK!');
+        break;
+    default:
+        console.log('Not this time!');
+        break;
+}
